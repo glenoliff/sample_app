07b7aaf19c8ec3326d4b7c50573ded4f617b84f8
glen <glen.oliff@gmail.com>
2015-01-16 16:15:39 +0000

    End of chapter 10 with exercises

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -7,16 +7,6 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     @non_admin = users(:archer)
   end
 
-  # test "index including pagination" do
-  #   log_in_as(@user)
-  #   get users_path
-  #   assert_template 'users/index'
-  #   assert_select 'div.pagination'
-  #   User.paginate(page: 1).each do |user|
-  #     assert_select 'a[href=?]', user_path(user), text: user.name
-  #   end
-  # end
-
   test "index as admin including pagination and delete links" do
     log_in_as(@admin)
     get users_path
@@ -24,8 +14,12 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     assert_select 'div.pagination'
     first_page_of_users = User.paginate(page: 1)
     first_page_of_users.each do |user|
-      assert_select 'a[href=?]', user_path(user), text: user.name
-      unless user == @admin
+      
+      unless !user.activated
+        assert_select 'a[href=?]', user_path(user), text: user.name
+      end
+      
+      unless user == @admin || !user.activated
         assert_select 'a[href=?]', user_path(user), text: 'delete',
                                                     method: :delete
       end
@@ -39,6 +33,13 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     log_in_as(@non_admin)
     get users_path
     assert_select 'a', text: 'delete', count: 0
+  end
+  
+  test "should get redirected when attempting to view a non-activated user" do
+    user = users(:lana)
+    log_in_as(@non_admin)
+    get user_path(user.id)
+    assert_redirected_to root_url
   end
 
 end
