e3e2f8bf5a967486769897cfde92d3fc89e27c32
glen <glen.oliff@gmail.com>
2015-01-16 13:56:32 +0000

    Add account activations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,9 +20,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     
     if @user.save
-      log_in @user
-      flash[:success] = 'Welcome to the Sample App!'
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
@@ -30,13 +30,10 @@ class UsersController < ApplicationController
   end
   
   def edit
-    #@user = User.find(params[:id])
   end
 
   def update
-    
-    #@user = User.find(params[:id])
-    
+
     if @user.update_attributes(user_params)
       flash[:success] = 'Profile Updated'
       redirect_to @user
