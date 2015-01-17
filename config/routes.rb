b7f7867618915f37307e2ef3b452c263d3004ff5
glen <glen.oliff@gmail.com>
2015-01-17 17:22:37 +0000

    Follow/Unfollow functionality complete

--- config/routes.rb
+++ config/routes.rb
@@ -20,10 +20,16 @@ Rails.application.routes.draw do
   post 'login'  => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- db/seeds.rb
+++ db/seeds.rb
@@ -30,8 +30,16 @@ end
 
 users = User.order(:created_at).take(6)
 
-50.times do
+40.times do
   content = Faker::Lorem.sentence(5)
   users.each { |u| u.microposts.create!(content: content) }
 end
+
+#Following relationships
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
 
